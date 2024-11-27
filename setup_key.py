2513dfccf09f2cb91d016d898bd27f0da2550443
git <bzhang90@jh.edu>
2024-11-27 13:36:19 -0500

    changed

--- setup_key.py
+++
@@ -1,4 +0,0 @@
-import openai
-
-# Set the API key directly
-openai.api_key = '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'
